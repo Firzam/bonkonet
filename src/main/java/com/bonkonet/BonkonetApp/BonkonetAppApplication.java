aaaf0454a0e329356fd9a7d284bc9a59ce3ca219
Grégory MOU KUI <gregory.moukui@viacesi.fr>
2020-10-15 15:53:46 +0200

    Adding hibernate specification Hello world

--- src/main/java/com/bonkonet/BonkonetApp/BonkonetAppApplication.java
+++ src/main/java/com/bonkonet/BonkonetApp/BonkonetAppApplication.java
@@ -11,9 +11,5 @@ public class BonkonetAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BonkonetAppApplication.class, args);
 	}
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
 
 }
--- src/main/java/com/bonkonet/BonkonetApp/Controllers/ClientController.java
+++ src/main/java/com/bonkonet/BonkonetApp/Controllers/ClientController.java
@@ -1,13 +1,21 @@
 package com.bonkonet.BonkonetApp.Controllers;
 
 import com.bonkonet.BonkonetApp.Services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/client")
 public class ClientController {
-
+    @Autowired
     private ClientService service;
+
+    @GetMapping()
+    public String test () {
+        return "Hello Yahya";
+    }
+
     
 }
